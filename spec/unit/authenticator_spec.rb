d5127f8d0df5aad4d4eefdbf6144550320e794a0
Zee Spencer <zee@zeespencer.com>
2015-02-09 20:02:45 -0800

    Use fancy-pants let/subject syntax

--- spec/unit/authenticator_spec.rb
+++ spec/unit/authenticator_spec.rb
@@ -4,30 +4,34 @@ require 'authenticator'
 describe Authenticator do
   let(:authenticator) { Authenticator.new(user_store) }
   let(:user_store) { double("Users") }
-  before() {
-    allow(user_store).to receive(:find_by).with({ email: email }).and_return(user)
-  }
-  describe("#authenticate") do
+
+  describe("#authenticate(email, access_token)") do
+    before() {
+      allow(user_store).to receive(:find_by).with({ email: email }).and_return(user)
+    }
+    subject(:result) { authenticator.authenticate(email, access_token) }
+
     context "When a user does not exist with the given email" do
       let(:email) { "not-found-user@example.com" }
       let(:user) { nil }
-      it "Returns false" do
-        expect(authenticator.authenticate(email, "asdf")).to eq(false)
-      end
+      let(:access_token) { "asdf" }
+
+      it { should eql(false) }
     end
 
     context "when the user exists with the given email" do
       let(:email) { "real-user@example.com"}
       let(:user) { double("FakeUser", access_token: "token") }
+
       context "and the access token provided is correct" do
-        it "returns the user" do
-          expect(authenticator.authenticate(email, "token")).to eql(user)
-        end
+        let(:access_token) { "token" }
+
+        it { should eql(user)}
       end
       context "and the access token provided is incorrect" do
-        it "returns false" do
-          expect(authenticator.authenticate(email, "wrong")).to eql(false)
-        end
+        let(:access_token) { "wrong" }
+
+        it { should eql(false) }
       end
     end
   end

897316c2bfbccc89638410a0c7285bfe3f1725c4
Zee Spencer <zee@zeespencer.com>
2015-02-09 19:57:09 -0800

    Use fancy-pants allow syntax

--- spec/unit/authenticator_spec.rb
+++ spec/unit/authenticator_spec.rb
@@ -3,25 +3,30 @@ require 'authenticator'
 
 describe Authenticator do
   let(:authenticator) { Authenticator.new(user_store) }
-  let(:user_store) { double("Users", find_by: user)}
+  let(:user_store) { double("Users") }
+  before() {
+    allow(user_store).to receive(:find_by).with({ email: email }).and_return(user)
+  }
   describe("#authenticate") do
     context "When a user does not exist with the given email" do
+      let(:email) { "not-found-user@example.com" }
       let(:user) { nil }
       it "Returns false" do
-        expect(authenticator.authenticate("foo@example.com", "asdf")).to eq(false)
+        expect(authenticator.authenticate(email, "asdf")).to eq(false)
       end
     end
 
     context "when the user exists with the given email" do
+      let(:email) { "real-user@example.com"}
       let(:user) { double("FakeUser", access_token: "token") }
       context "and the access token provided is correct" do
         it "returns the user" do
-          expect(authenticator.authenticate("foo@example.com", "token")).to eql(user)
+          expect(authenticator.authenticate(email, "token")).to eql(user)
         end
       end
       context "and the access token provided is incorrect" do
         it "returns false" do
-          expect(authenticator.authenticate("foo@example.com", "wrong")).to eql(false)
+          expect(authenticator.authenticate(email, "wrong")).to eql(false)
         end
       end
     end
